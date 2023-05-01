6018b5c6bad48989e7b1ee454baa99b36c88c773
kazu5150 <kazu.deadtotheworld@gmail.com>
2023-05-02 06:45:32 +0900

    update decorator 2files

--- decorator.py
+++ decorator.py
@@ -13,8 +13,14 @@ def say_name(name):
 
 @greeting
 def say_name_origin(name, origin):
-    print(f"I'm {name} ,I'm from {origin}")
+    print(f"I'm {name}. ,I'm from {origin}")
+
+
+@greeting
+def say_name_dob_origin(name, origin, dob):
+    print(f"I'm {name}. I'm from {origin}. I was born in {dob}.")
 
 
 # say_name("kazu")
-say_name_origin("kazu", "nagano")
+# say_name_origin("kazu", "nagano")
+say_name_dob_origin("kazu", "nagano", "1963/12/19")
